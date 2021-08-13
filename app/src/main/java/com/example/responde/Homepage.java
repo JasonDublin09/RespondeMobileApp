0f35d252baea73012956cc79ab88f8ecfd893f14
ninoasistio <ninojay.asistio.iics@ust.edu.ph>
2021-08-13 21:01:59 +0800

    Jay-Z Added my location page and redirecting buttons with confirm toast and cancel button

--- app/src/main/java/com/example/responde/Homepage.java
+++ app/src/main/java/com/example/responde/Homepage.java
@@ -2,13 +2,31 @@ package com.example.responde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Homepage extends AppCompatActivity {
-
+private ImageButton myloc_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        myloc_btn = (ImageButton) findViewById(R.id.myloc_btn);
+        myloc_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openmy_location();
+            }
+        });
+
+    }
+
+    public void openmy_location(){
+        Intent intent = new Intent(this, my_location.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/responde/my_location.java
@@ -0,0 +1,45 @@
+package com.example.responde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class my_location extends AppCompatActivity {
+private Button cancel_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_location);
+
+        cancel_btn = (Button) findViewById(R.id.cancel_btn);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomepage();
+            }
+        });
+
+    }
+
+    public void openHomepage(){
+        Intent intent = new Intent(this, Homepage.class);
+        startActivity(intent);
+    }
+
+
+    public void toastMsg(String msg) {
+
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("Report Sent to the Fire District V");
+    }
+
+}
