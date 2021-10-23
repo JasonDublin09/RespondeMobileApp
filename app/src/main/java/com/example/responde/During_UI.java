2396bb2a79e40c53fcae8b524e705e8c49dfaba2
ninoasistio <ninojay.asistio.iics@ust.edu.ph>
2021-10-23 22:35:51 +0800

    Fixed all the what to do texts and Backgrounds

--- app/src/main/java/com/example/responde/During_UI.java
+++ app/src/main/java/com/example/responde/During_UI.java
@@ -6,23 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class During_UI extends AppCompatActivity {
 
     Button backBtn;
+    TextView textout;
+    StringBuilder sb = new StringBuilder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_during_ui);
 
         backBtn = findViewById(R.id.back);
+        textout = (TextView) findViewById(R.id.whatododuringtext);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(During_UI.this, Whattodo.class));
             }
+
+
         });
 
+        }
     }
-}
